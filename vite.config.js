0f05334ce78604b69d7c33912629129422756a55
lauechev <laura.echeverri3@gmail.com>
2025-02-03 16:32:43 -0500

    ☘️ arreglar vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
   base: '/desert-greens/',
   server: {
     port: 3000,
   },
-  publicDir: 'estaticos',
+  publicDir: 'assets',
   build: {
     outDir: 'publico',
-    assetsDir: 'estaticos',
+    assetsDir: 'assets',
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        home: fileURLToPath(new URL('./index.html', import.meta.url)),
+        about: fileURLToPath(new URL('./about.html', import.meta.url)),
+      },
+    },
   },
   css: {
     preprocessorOptions: {
